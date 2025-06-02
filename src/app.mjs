a4225432a8417112ffbec9483f8b55f4e50ba0bc
Kwabena Anane Kwarteng <kwabena.ananeK@gmail.com>
2025-06-02 03:44:53 +0200

    Update to be able to write to blockchain.json file

--- src/app.mjs
+++ src/app.mjs
@@ -3,18 +3,20 @@ import dotenv from 'dotenv';
 import errorHandler from './middleware/errorHandler.mjs';
 import { logger } from './middleware/logger.mjs';
 import AppError from './models/appError.mjs';
+import blockchainRoutes from './routes/blockchainRoutes.mjs';
 
 dotenv.config({ path: './config/config.env' });
-
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(logger);
 }
 
-// app.use('/api/books', booksRouter);
+app.use('/api', blockchainRoutes);
+
+app.use(errorHandler);
 
 app.all('*', (req, res, next) => {
   next(
@@ -24,7 +26,5 @@ app.all('*', (req, res, next) => {
     )
   );
 });
-
-app.use(errorHandler);
 
 export { app };
